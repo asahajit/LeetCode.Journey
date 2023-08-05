485a481edd077dbe4b2458940c8cb815e79177bc
Asahajit Dalui <jeet.dalui555@gmail.com>
2023-08-05 12:02:30 +0530

    1768. Merge Strings Alternately

---
+++ LeetCode/Easy/Merge_Strings_Alternately.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public static class Merge_Strings_Alternately
+    {
+        public static string MergeAlternately(string word1, string word2)
+        {
+            int maxlength = word1.Length > word2.Length ? word1.Length : word2.Length;
+            int minlength = word1.Length < word2.Length ? word1.Length : word2.Length;
+            string result = string.Empty;
+
+            for (int i = 0; i < maxlength; i++)
+            {
+                if (word1.Length >= minlength && word1.Length <= maxlength && word1.Length>i)
+                {
+                    result = result+word1.Substring(i,1);
+                }
+                if (word2.Length >= minlength && word2.Length <= maxlength && word2.Length > i)
+                {
+                    result = result + word2.Substring(i, 1);
+                }
+            }
+            return result;
+
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -20,11 +20,13 @@ namespace LeetCode
             //Console.WriteLine(Palindrom.IsPalindrome(1122));
             //Console.WriteLine(Palindrom.IsPalindrome(55655));
             //Console.WriteLine(Palindrom.IsPalindrome(555555));
-            Console.WriteLine(Timeconvert.timeConversion("07:05:45PM"));
-            Console.WriteLine(Timeconvert.timeConversion("06:40:03AM"));
-            Console.WriteLine(Timeconvert.timeConversion("01:05:45AM"));
-            Console.WriteLine(Timeconvert.timeConversion("12:00:00AM"));
-            Console.WriteLine(Timeconvert.timeConversion("12:45:54PM"));
+            //Console.WriteLine(Timeconvert.timeConversion("07:05:45PM"));
+            //Console.WriteLine(Timeconvert.timeConversion("06:40:03AM"));
+            //Console.WriteLine(Timeconvert.timeConversion("01:05:45AM"));
+            //Console.WriteLine(Timeconvert.timeConversion("12:00:00AM"));
+            //Console.WriteLine(Timeconvert.timeConversion("12:45:54PM"));
+            Console.WriteLine(Merge_Strings_Alternately.MergeAlternately("pqrs", "ab"));
+
 
 
             Console.ReadLine();
